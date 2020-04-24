05905b296d4670677e55e0a9d6821f496411a1eb
aldrich23 <m.ralphaldrich@gmail.com>
2020-04-24 19:13:46 +0800

    Updates

---
+++ precache-manifest.9a4c6639334218b8d41c786c9a3c4281.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aa74b4671d4fcd2ee9c76989aa2f87f",
+    "url": "/aliens-list/index.html"
+  },
+  {
+    "revision": "a39666eba8234e3f0f8d",
+    "url": "/aliens-list/static/css/main.a142fb8a.chunk.css"
+  },
+  {
+    "revision": "c25b9d18429ab775fa10",
+    "url": "/aliens-list/static/js/2.5a9b995e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/aliens-list/static/js/2.5a9b995e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a39666eba8234e3f0f8d",
+    "url": "/aliens-list/static/js/main.e8ff65b8.chunk.js"
+  },
+  {
+    "revision": "fdc78c8157eb38bbf3ce",
+    "url": "/aliens-list/static/js/runtime-main.63f2e424.js"
+  }
+]);
